4867a20fa5d1ffe79ff47e6cc866e0389a03803a
ikanago <28985004+ikanago@users.noreply.github.com>
2020-06-05 21:57:06 +0900

    Zoom in to face

--- dataset/setup_facesdb.py
+++ dataset/setup_facesdb.py
@@ -15,9 +15,9 @@ def crop_center(file_path, crop_width, crop_height):
     image = Image.open(file_path)
     img_width, img_height = image.size
     return image.crop(((img_width - crop_width) // 2,
-                       (img_height - crop_height) // 2,
+                       (img_height - crop_height) // 2 + 30,
                        (img_width + crop_width) // 2,
-                       (img_height + crop_height) // 2))
+                       (img_height + crop_height) // 2 + 30))
 
 
 def create_dataset():
@@ -33,13 +33,14 @@ def create_dataset():
 
     train_dict = {"Class Label": [], "File Path": []}
     test_dict = {"Class Label": [], "File Path": []}
+    image_length = 250
     for emotion_id in range(7):
         (train_data_index, test_data_index) = randomly_split_data(emotion_id)
         for person_id_train in train_data_index:
             file_name_train = "s{0:03d}-{1:02d}_img.bmp".format(person_id_train, emotion_id)
             file_path = os.path.join(root_dir, "s{0:03d}".format(person_id_train), "bmp", file_name_train)
             # 両側の黒い部分を切り取る
-            croped_image = crop_center(file_path, 480, 480)
+            croped_image = crop_center(file_path, image_length, image_length)
             croped_image.save(os.path.join(train_data_dir, file_name_train))
             train_dict["Class Label"].append(put_label(emotion_id))
             train_dict["File Path"].append(os.path.join("train_data/", file_name_train))
@@ -48,7 +49,7 @@ def create_dataset():
             file_name_test = "s{0:03d}-{1:02d}_img.bmp".format(person_id_test, emotion_id)
             file_path = os.path.join(root_dir, "s{0:03d}".format(person_id_test), "bmp", file_name_test)
             # 両側の黒い部分を切り取る
-            croped_image = crop_center(file_path, 480, 480)
+            croped_image = crop_center(file_path, image_length, image_length)
             croped_image.save(os.path.join(test_data_dir, file_name_test))
             test_dict["Class Label"].append(put_label(emotion_id))
             test_dict["File Path"].append(os.path.join("test_data/", file_name_test))
