5dea37a502bb5142679127d166373d047a18c040
ikanago <28985004+ikanago@users.noreply.github.com>
2020-06-08 12:33:47 +0900

    hoge

--- cnn.py
+++ cnn.py
@@ -26,12 +26,12 @@ class myCNN(nn.Module):
         # 層の定義： ここでは 畳込み層 2 つ，プーリング層 2 つ，全結合層 2 つ とする
         self.conv1 = Conv(in_channels=img_channels, out_channels=self.L1_CHANNELS, kernel_size=30, activation=F.relu)
         self.conv2 = Conv(in_channels=self.L1_CHANNELS, out_channels=self.L2_CHANNELS, kernel_size=30, activation=F.relu)
-        # self.conv3 = Conv(in_channels=self.L2_CHANNELS, out_channels=self.L3_CHANNELS, kernel_size=30, activation=F.relu)
-        # self.conv4 = Conv(in_channels=self.L3_CHANNELS, out_channels=self.L4_CHANNELS, kernel_size=30, activation=F.relu)
+        self.conv3 = Conv(in_channels=self.L2_CHANNELS, out_channels=self.L3_CHANNELS, kernel_size=30, activation=F.relu)
+        self.conv4 = Conv(in_channels=self.L3_CHANNELS, out_channels=self.L4_CHANNELS, kernel_size=30, activation=F.relu)
         self.pool = Pool(method='max')
-        self.w = img_width // 4 # プーリング層を 2 回経由するので，特徴マップの横幅は 1/2^2 = 1/4 となる
-        self.h = img_height // 4 # 縦幅についても同様
-        self.nz = self.w * self.h * self.L2_CHANNELS # 全結合層の直前におけるユニットの総数
+        self.w = img_width //  16 # プーリング層を 2 回経由するので，特徴マップの横幅は 1/2^2 = 1/4 となる
+        self.h = img_height // 16 # 縦幅についても同様
+        self.nz = self.w * self.h * self.L4_CHANNELS # 全結合層の直前におけるユニットの総数
         self.fc3 = FC(in_units=self.nz, out_units=self.L4_UNITS, activation=F.relu)
         self.fc4 = FC(in_units=self.L4_UNITS, out_units=out_units, activation=None) # 最終層では一般に活性化関数は入れない
 
@@ -43,10 +43,10 @@ class myCNN(nn.Module):
         h = self.pool(h)
         h = self.conv2(h)
         h = self.pool(h)
-        # h = self.conv3(h)
-        # h = self.pool(h)
-        # h = self.conv4(h)
-        # h = self.pool(h)
+        h = self.conv3(h)
+        h = self.pool(h)
+        h = self.conv4(h)
+        h = self.pool(h)
         h = h.view(h.size()[0], -1) # 平坦化：特徴マップを一列に並べ直す（FC層の直前では必ず必要）
         h = self.fc3(h)
         y = self.fc4(h)
--- dataset/setup_facesdb.py
+++ dataset/setup_facesdb.py
@@ -17,9 +17,9 @@ def crop_center(file_path, crop_width, crop_height):
     horizontal_offset = random.randint(-5, 5) + 25
     vertical_offset = random.randint(-5, 5)
     return image.crop(((img_width - crop_width) // 2,
-                       (img_height - crop_height) // 2 + 25,
+                       (img_height - crop_height) // 2,
                        (img_width + crop_width) // 2,
-                       (img_height + crop_height) // 2 + 25))
+                       (img_height + crop_height) // 2))
 
 
 def create_dataset():
@@ -35,8 +35,8 @@ def create_dataset():
 
     train_dict = {"Class Label": [], "File Path": []}
     test_dict = {"Class Label": [], "File Path": []}
-    image_length = 240
-    copy_num = 4
+    image_length = 480
+    copy_num = 6
     for emotion_id in range(7):
         (train_data_index, test_data_index) = randomly_split_data(emotion_id)
         for person_id_train in train_data_index:
@@ -47,7 +47,7 @@ def create_dataset():
                 croped_image = crop_center(file_path, image_length, image_length)
                 output_file_train = "s{0:03d}-{1:02d}_{2}img.bmp".format(person_id_train, emotion_id, i)
                 croped_image.save(os.path.join(train_data_dir, output_file_train))
-                train_dict["Class Label"].append(emotion_id)
+                train_dict["Class Label"].append(put_label(emotion_id))
                 train_dict["File Path"].append(os.path.join("train_data/", output_file_train))
 
         for person_id_test in test_data_index:
@@ -56,7 +56,7 @@ def create_dataset():
             # 両側の黒い部分を切り取る
             croped_image = crop_center(file_path, image_length, image_length)
             croped_image.save(os.path.join(test_data_dir, file_name_test))
-            test_dict["Class Label"].append(emotion_id)
+            test_dict["Class Label"].append(put_label(emotion_id))
             test_dict["File Path"].append(os.path.join("test_data/", file_name_test))
 
     df = pd.DataFrame(data=train_dict)
@@ -70,10 +70,10 @@ def randomly_split_data(emotion_id):
         if i != 20 and i != 22:
             person_labels.append(i)
     random.shuffle(person_labels)
-    # if emotion_id == 2 or emotion_id == 4 or emotion_id == 5 or emotion_id == 6:
-    #     return (person_labels[0:6], person_labels[6:9])
-    # else:
-    return (person_labels[0:24], person_labels[24:])
+    if emotion_id == 2 or emotion_id == 4 or emotion_id == 5 or emotion_id == 6:
+        return (person_labels[0:6], person_labels[6:9])
+    else:
+        return (person_labels[0:24], person_labels[24:])
 
 def put_label(n):
     assert(0 <= n < 7)
